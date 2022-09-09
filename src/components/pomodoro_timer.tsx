6c8ea47bbcc494f9c20379d818ad792df5982920
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-09 14:32:58 -0300

    sounds

--- src/components/pomodoro_timer.tsx
+++ src/components/pomodoro_timer.tsx
@@ -4,6 +4,14 @@ import { secondsToTime } from '../utils/seconds_to_time';
 import { Button } from './button';
 import { Timer } from './timer';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellStart = require('../sounds/bell-start.mp3');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bellFinish = require('../sounds/bell-finish.mp3');
+
+const audioStartWorking = new Audio(bellStart);
+const audioStopWorking = new Audio(bellFinish);
+
 interface Props {
   pomodoroTime: number;
   shortRestTime: number;
@@ -34,6 +42,7 @@ export function PomodoroTimer(props: Props): JSX.Element {
     setWorking(true);
     setResting(false);
     setMainTime(props.pomodoroTime);
+    audioStartWorking.play();
   };
 
   const configureRest = (long: boolean) => {
@@ -46,6 +55,8 @@ export function PomodoroTimer(props: Props): JSX.Element {
     } else {
       setMainTime(props.shortRestTime);
     }
+
+    audioStopWorking.play();
   };
 
   return (
